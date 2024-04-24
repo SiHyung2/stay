7bf1152286023eccf0fc717c87a3b94f20a5c16a
it <tlgud200@naver.com>
2024-04-24 16:09:10 +0900

    테스트-sh

--- stay/src/main/java/com/example/service/AccommodationService.java
+++ stay/src/main/java/com/example/service/AccommodationService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.example.domain.AccommodationDTO;
 
 public interface AccommodationService {
-	
+	 
 	public List<AccommodationDTO> getList();
 	public void insertaccommodation(AccommodationDTO accommdation);
 	public void updateaccommodation(AccommodationDTO accommdation);
